755ea50892d08beec96d13fc470dee000e9a5aea
Moses Turner <moses@collabora.com>
2022-07-30 00:37:37 +0100

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,83 @@
 #include <stdio.h>
 #include "videoInput.h"
 
+#include <fstream>
+#include <string>
+
+// #include "monado/src/xrt/include/xrt/xrt_config.h"
+#include "xrt/xrt_config.h"
+
+#include "vive/vive_config.h"
+
+#include "tracking/t_hand_tracking.h"
+#include "tracking/t_frame_cv_mat_wrapper.hpp"
+#include "../monado/src/xrt/tracking/hand/mercury/hg_interface.h"
+
+#include "os/os_time.h"
+
+namespace xat = xrt::auxiliary::tracking;
+
+std::string read_file(std::string_view path) {
+    constexpr auto read_size = std::size_t(4096);
+    auto stream = std::ifstream(path.data());
+    stream.exceptions(std::ios_base::badbit);
+    
+    std::string out = std::string();
+    auto buf = std::string(read_size, '\0');
+    while (stream.read(& buf[0], read_size)) {
+        out.append(buf, 0, stream.gcount());
+    }
+    out.append(buf, 0, stream.gcount());
+    return out;
+}
+
+
+
 int main() {
+
+	std::string t20_config = read_file("C:\\dev\\ht_windows_steamvr_2\\T20_config.json");
+
+	std::cout << t20_config << std::endl;
+
+
+	struct vive_config c = {};
+
+	const char* e = t20_config.c_str();
+
+	char* j = (char*)malloc(sizeof(char) * 500000);
+
+	strcpy(j, e);
+
+	vive_config_parse(&c, j, U_LOGGING_DEBUG);
+
+	struct t_stereo_camera_calibration *calib = NULL;
+
+	xrt_pose head_in_left; // unused
+
+	vive_get_stereo_camera_calibration(&c, &calib, &head_in_left);
+
+
+struct t_image_boundary_info info;
+	info.views[0].type = HT_IMAGE_BOUNDARY_CIRCLE;
+	info.views[1].type = HT_IMAGE_BOUNDARY_CIRCLE;
+
+
+	//!@todo This changes by like 50ish pixels from device to device. For now, the solution is simple: just
+	//! make the circle a bit bigger than we'd like.
+	// Maybe later we can do vignette calibration? Write a tiny optimizer that tries to fit Index's
+	// gradient? Unsure.
+	info.views[0].boundary.circle.normalized_center.x = 0.5f;
+	info.views[0].boundary.circle.normalized_center.y = 0.5f;
+
+	info.views[1].boundary.circle.normalized_center.x = 0.5f;
+	info.views[1].boundary.circle.normalized_center.y = 0.5f;
+
+	info.views[0].boundary.circle.normalized_radius = 0.55;
+	info.views[1].boundary.circle.normalized_radius = 0.55;
+
+struct t_hand_tracking_sync * sync =
+	t_hand_tracking_sync_mercury_create(calib, info);
+
 
 	videoInput in;
 
@@ -34,6 +110,7 @@ int main() {
 		}
 	}
 	if (!is_camera) {
+		std::cout << "Didn't find camera!" << std::endl;
 		abort();
 	}
 
@@ -61,28 +138,41 @@ int main() {
 		in.getPixels(wanted_idx, buf, false, true);
 		//cv::Mat(full_size, CV_8UC3, debug_frame->data, debug_frame->stride);
 
+		uint64_t time = os_monotonic_get_ns();
+
 
 
 		cv::Mat eh = cv::Mat(cv::Size(1920, 960), CV_8UC3, buf, 1920*3);
-		cv::imshow("h", eh);
-		cv::waitKey(1);
+
 		std::cout << "lol\n";
+
+		cv::Mat mats_rgb[2];
+		cv::Mat mats_grayscale[2];
+
+		// xat::FrameMat fms[2]; // = {};
+		xrt_frame *frames[2] = {NULL, NULL};
+
+		mats_rgb[0] = eh(cv::Rect(0,0,960,960));
+		mats_rgb[1] = eh(cv::Rect(960, 0, 960, 960));
+
+		for (int i = 0; i < 2; i++) {
+			cv::cvtColor(mats_rgb[i], mats_grayscale[i], cv::COLOR_BGR2GRAY);
+			xat::FrameMat::Params params;
+			params.stereo_format = XRT_STEREO_FORMAT_NONE;
+			params.timestamp_ns = time;
+			xat::FrameMat::wrapL8(mats_grayscale[i], &frames[i], params);
+
+		}
+		struct xrt_hand_joint_set hands[2];
+		uint64_t out_timestamp;
+t_ht_sync_process(sync, frames[0], frames[1], &hands[0], &hands[1], &out_timestamp);
+
+		// 		cv::imshow("0", mats_grayscale[0]);
+		// 		cv::imshow("l", mats_grayscale[1]);
+		// cv::waitKey(1);
 	}
 
 	return 0;
 
-
-
-	cv::VideoCapture bleh;
-	bleh.open(0);
-
-	while (true) {
-		cv::Mat img;
-		bleh.read(img);
-
-		cv::imshow("h", img);
-		cv::waitKey(1);
-	}
-	return 0;
 }
 
